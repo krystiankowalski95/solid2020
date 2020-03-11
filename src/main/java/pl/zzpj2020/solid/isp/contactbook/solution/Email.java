fa2db78421549892d33c5f9a912e8959623cd9b1
Krystian Kowalski <49548559+krystiankowalski95@users.noreply.github.com>
2020-03-11 21:53:47 +0100

    Fix#2

--- src/main/java/pl/zzpj2020/solid/isp/contactbook/solution/Email.java
+++ src/main/java/pl/zzpj2020/solid/isp/contactbook/solution/Email.java
@@ -1,6 +1,5 @@
 package pl.zzpj2020.solid.isp.contactbook.solution;
 
 public interface Email {
-    void sendMessage(String subject, String body);
     void sendEmail(String emailAddress, String subject, String body);
 }
