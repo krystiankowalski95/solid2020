4f3e232feba4184c1d80b7434fc9e9bdf7beb41b
Erwing <erwingdelfines@hotmail.com>
2021-06-28 20:10:31 -0500

    OCP solved

--- src/solid/ocp/usa/violation/USASpeedLimitFines.java
+++ src/solid/ocp/usa/violation/USASpeedLimitFines.java
@@ -1,37 +1,14 @@
 package pl.zzpj2020.solid.ocp.usa.violation;
 
-public class USASpeedLimitFines {
+public class USASpeedLimitFineCalculator {
 
-	private static final int SC_MAX_SPEED = 0;
-	private static final int AL_MAX_SPEED = 0;
-	private static final int GA_MAX_SPEED = 0;
-
-	public double calcualateSpeedLimitFine(String stateCode, int speed) {
+	
+	public static double calcualateSpeedLimitFine(StateFine stateFine, float speed) {
 
 		double fine = 0;
-		switch (stateCode) {
-		case "SC": {
-			if (speed > SC_MAX_SPEED) {
-				// calculate...
-			}
-			return fine;
+		if(speed>stateFine.getMAX_SPEED() ){
+			fine= stateFine.getFINE_VALUE();
 		}
-		case "AL": {
-			if (speed > AL_MAX_SPEED) {
-				// calculate...
-			}
-			return fine;
-		}
-		case "GA": {
-			if (speed > GA_MAX_SPEED) {
-				// calculate...
-			}
-			return fine;
-		} 
-
-		}
-		return 0.0;
-
-	}
+		return fine;
 
 }
